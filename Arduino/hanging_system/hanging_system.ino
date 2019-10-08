a56313f124f7f9467b0d7bd9fc271299e34732bd
morita-daiki <37872526+Morita-Daiki@users.noreply.github.com>
2019-10-08 21:42:47 +0900

    説明追加＆無駄削除

--- Arduino/hanging_system/hanging_system.ino
+++ Arduino/hanging_system/hanging_system.ino
@@ -1,73 +1,67 @@
-#include <Servo.h> //サーボ使うよ
-#include "hanging_system.h"
-#include <SoftwareSerial.h>
+#include <Servo.h>          //サーボ使うよ
+#include "hanging_system.h" //かける機構ライブラリ
+#include <SoftwareSerial.h> //MDグリーンへの送信用ソフトウェアシリアル
 
 //初期位置，供給位置
-const int degpA[2] = {90, 0};
-const int degmA[2] = {90, 0};
-const int degpB[2] = {90, 0};
-const int degmB[2] = {90, 0};
+const int degpA[2] = {90, 0}; //サーボA-plus
+const int degmA[2] = {90, 0}; //サーボA-minus
+const int degpB[2] = {90, 0}; //サーボA-plus
+const int degmB[2] = {90, 0}; //サーボA-minus
 
+//上の時の供給，下の時の供給,エアー，上の供給角度
 //spp,spm,air,degpdef,degmdef,t1,t2,tc
 hanging_system unit_A(2, 3, 11, degpA[0], degmA[0], 500, 300, 1000);
 hanging_system unit_B(4, 5, 12, degpB[0], degmB[0], 500, 300, 1000);
 
-#define SAFETYTIME 100
+SoftwareSerial mdblack(A1, A0); //rx,txモタドらへの送信
 
-SoftwareSerial mdblack(A1, A0); //rx,tx
+uint8_t mdblack_datas[4]; //モタドらへの送信データ
 
-unsigned long last_time = 0;
-
-uint8_t mdblack_datas[4];
-
-boolean now_button_state[15];
-boolean last_button_state[15];
-boolean button_rize[15];
-int sta13 = 0;
+boolean now_button_state[15];  //現在ボタン
+boolean last_button_state[15]; //1個前ボタン
+boolean button_rize[15];       //ボタンが立ち上がったか
+int sta13 = 0;                 //LEDちかちか用
 void setup()
 {
-   pinMode(13, OUTPUT);
-   pinMode(8, OUTPUT);
-   digitalWrite(8, HIGH);
-   digitalWrite(13, HIGH);
-   delay(1000);
-   digitalWrite(13, LOW);
-   Serial.begin(115200);
-   mdblack.begin(9600);
+   pinMode(8, OUTPUT);    //起動確認LED
+   pinMode(13, OUTPUT);   //シリアル確認LED
+   digitalWrite(8, HIGH); //起動確認LED
+   digitalWrite(13, LOW); //シリアル確認LED
+   Serial.begin(115200);  //XBee通信速度
+   mdblack.begin(9600);   //モタドら通信速度
 }
 
 void loop()
 {
-   if (Serial.available() > 0)
+   if (Serial.available() > 0) //シリアルが受信可能時
    {
-      if (Serial.read() == 0xff)
+      if (Serial.read() == 0xff) //ヘッダーデータ来てたら
       {
-         digitalWrite(13, sta13 % 2);
-         sta13++;
-         uint8_t values[19];
-         Serial.readBytes(values, 19);
-         last_time = millis();
+         digitalWrite(13, sta13); //シリアル状態LED更新
+         sta13 = !sta13;          //シリアル状態更新
+
+         uint8_t values[19];           //受信データ格納配列
+         Serial.readBytes(values, 19); //データ読みこみ
 
          //ボタン現状態と立ち上がり検知
-         for (int i = 0; i < 15; i++)
+         for (int i = 0; i < 15; i++) //0~14はWiiのボタンデータ
          {
             now_button_state[i] = (values[i] > 0);                         //現在ボタン更新(>0なのは，ZやLボタンが0~16みたいな値を持つから)
             button_rize[i] = !last_button_state[i] && now_button_state[i]; //前０で今１だと立ち上がり
             last_button_state[i] = now_button_state[i];                    //今を過去に
          }
 
-         mdblack_datas[0] = values[15];
-         mdblack_datas[1] = values[16];
-         mdblack_datas[2] = values[17];
-
+         mdblack_datas[0] = values[15];                  //LX
+         mdblack_datas[1] = values[16];                  //LY
+         mdblack_datas[2] = values[17];                  //XR
          mdblack_datas[3] = (button_rize[0] << 0) |      //開閉
                             (now_button_state[7] << 1) | //出っ張る
                             (now_button_state[9] << 2) | //縮む
                             (now_button_state[8] << 3) | //↑
                             (now_button_state[10] << 4); //↓
 
-         mdblack.listen();
-         mdblack.write(0xff);
+         mdblack.listen();    //ソフトウェアシリアルを使えるように
+         mdblack.write(0xff); //ヘッダ送信
          mdblack.write(mdblack_datas, 4);
          // send_bytes(mdblack, send_datas, 5); //モーターとエアのデータ送信
 
@@ -78,7 +72,6 @@ void loop()
                unit_A.hang(0, degmA[1], degmA[0]); //奥　にかけて上がる
             else if (button_rize[13])              //R
                unit_A.hang(1, degpA[1], degpA[0]); //手前にかけて上がる
-
             //左
             if (button_rize[12])                   //ZL
                unit_B.hang(0, degmB[1], degmB[0]); //奥　にかけて上がる
@@ -92,7 +85,6 @@ void loop()
                unit_A.empty_hang(0);  //奥　が上がる
             else if (button_rize[13]) //R
                unit_A.empty_hang(1);  //手前が上がる
-
             //左
             if (button_rize[12])      //ZL
                unit_B.empty_hang(0);  //奥　が上がる
@@ -101,7 +93,6 @@ void loop()
          }
       }
    }
-   unit_A.always_run_function();
-   unit_B.always_run_function();
-   // delay(1);
+   unit_A.always_run_function(); //ハンギングシステムの常時更新
+   unit_B.always_run_function(); //ハンギングシステムの常時更新
 }
