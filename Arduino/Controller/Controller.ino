6167c0e480fd5d8939db8f7b903c820763ca45a0
morita-daiki <37872526+Morita-Daiki@users.noreply.github.com>
2019-10-08 09:08:11 +0900

    コメント追加

--- Arduino/Controller/Controller.ino
+++ Arduino/Controller/Controller.ino
@@ -1,51 +1,50 @@
 //Morita Daiki 2019 @NIT.yonago
 //マイコン:arduino pro mini 互換（自作）
-#include <WiiChuck.h>
+#include <WiiChuck.h> //Wiiのライブラリを使う
 
-//つないだアクセサリーの名前をWiiとする
-Accessory Wii;
-int sta13 = 0;
+Accessory Wii; //つないだアクセサリーの名前をWiiとする
 
-//設定
-void setup()
+boolean sta13 = 0; //LED状態
+
+void setup() //設定
 {
-  Serial.begin(115200); //シリアルボートレート
-  pinMode(13, OUTPUT);
+  Serial.begin(115200);    //シリアルボートレート
+  pinMode(13, OUTPUT);     //13番ピンを出力に
   Wii.begin();             //コントローラ接続
   if (Wii.type == Unknown) //Wiiの種類が分からないとき
     Wii.type = WIICLASSIC; //クラシックコントローラーだよ
 }
 void loop()
 {
-  digitalWrite(13, sta13 % 2);
-  sta13++;
-  //長さWII_VALUES_ARRAY_SIZE(=19)のwii_dataという配列を用意
-  uint8_t wii_data[WII_VALUES_ARRAY_SIZE];
+  digitalWrite(13, sta13); //LED更新
+  sta13 = !sta13;          //LED状態切り替え
 
-  Wii.readData();
+  uint8_t wii_data[WII_VALUES_ARRAY_SIZE]; //長さWII_VALUES_ARRAY_SIZE(=19)のwii_dataという配列を用意
+
+  Wii.readData(); //Wiiデータ取得
 
   //配列の各要素に値を代入
-  wii_data[0] = Wii.getButtonA();
-  wii_data[1] = Wii.getButtonX();
-  wii_data[2] = Wii.getButtonY();
-  wii_data[3] = Wii.getButtonB();
-  wii_data[4] = Wii.getPlusButton();
-  wii_data[5] = Wii.getButtonHome();
-  wii_data[6] = Wii.getButtonMinus();
-  wii_data[7] = Wii.getPadRight();
-  wii_data[8] = Wii.getPadUp();
-  wii_data[9] = Wii.getPadLeft();
-  wii_data[10] = Wii.getPadDown();
-  wii_data[11] = Wii.getTriggerLeft();
-  wii_data[12] = Wii.getButtonZLeft() > 0;
-  wii_data[13] = Wii.getTriggerRight();
-  wii_data[14] = Wii.getButtonZRight() > 0;
-  wii_data[15] = Wii.getJoyXLeft();
-  wii_data[16] = Wii.getJoyYLeft();
-  wii_data[17] = Wii.getJoyXRight();
-  wii_data[18] = Wii.getJoyYRight();
+  wii_data[0] = Wii.getButtonA();           //0,1
+  wii_data[1] = Wii.getButtonX();           //0,1
+  wii_data[2] = Wii.getButtonY();           //0,1
+  wii_data[3] = Wii.getButtonB();           //0,1
+  wii_data[4] = Wii.getPlusButton();        //0,1
+  wii_data[5] = Wii.getButtonHome();        //0,1
+  wii_data[6] = Wii.getButtonMinus();       //0,1
+  wii_data[7] = Wii.getPadRight();          //0,1
+  wii_data[8] = Wii.getPadUp();             //0,1
+  wii_data[9] = Wii.getPadLeft();           //0,1
+  wii_data[10] = Wii.getPadDown();          //0,1
+  wii_data[11] = Wii.getTriggerLeft();      //0,1
+  wii_data[12] = Wii.getButtonZLeft() > 0;  //0,16(0,32?)みたいなデータを1,0に変換
+  wii_data[13] = Wii.getTriggerRight();     //0,1
+  wii_data[14] = Wii.getButtonZRight() > 0; //0,16(0,32?)みたいなデータを1,0に変換
+  wii_data[15] = Wii.getJoyXLeft();         //0~63
+  wii_data[16] = Wii.getJoyYLeft();         //0~63
+  wii_data[17] = Wii.getJoyXRight();        //0~31
+  wii_data[18] = Wii.getJoyYRight();        //0~31
 
   Serial.write(0xff);         //ヘッダ＝11111111
   Serial.write(wii_data, 19); //19個のデータ送信
-  delay(20);                  //５ｍｓ待つ
+  delay(20);                  //20ms待つ
 }
